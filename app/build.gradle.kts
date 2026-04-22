0282d4bdb79c272bff0b60f6219011f269663867
moonsetter7 <ivandaniel491.r@gmail.com>
2026-04-22 17:54:32 +0100

    Added room+ksp dependencies
    > Added room dependencies
    > Added ksp as annotation processor for room
      > migrated to ksp using this guide:
      https://developer.android.com/build/migrate-to-ksp
    > Migrated to version catalogs
      > Followed this guide:
      https://developer.android.com/build/migrate-to-catalogs

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -53,4 +54,8 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,13 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
+buildscript {
+    extra.apply {
+        set("nav_version", "2.8.4")
+        set("room_version", "2.5.0")
+    }
+}
+
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.kotlin.compose) apply false
+    id("com.google.devtools.ksp") version "2.3.4" apply false
 }
