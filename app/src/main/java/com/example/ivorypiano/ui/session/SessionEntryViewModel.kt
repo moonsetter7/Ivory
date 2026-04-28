26766bd3dd1d0bf42f9704bf7dfec27903fc7da2
moonsetter7 <ivandaniel491.r@gmail.com>
2026-04-28 18:59:06 +0100

    added more functionality to SessionEntryViewModel
    commiting unfinished changes to move laptop work to pc
    never using android studio on my laptop again

--- app/src/main/java/com/example/ivorypiano/ui/session/SessionEntryViewModel.kt
+++ app/src/main/java/com/example/ivorypiano/ui/session/SessionEntryViewModel.kt
@@ -1,6 +1,20 @@
 package com.example.ivorypiano.ui.session
 
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
 import com.example.ivorypiano.data.PianoSession
+import com.example.ivorypiano.data.SessionsRepository
+
+/**
+ * ViewModel to validate and insert sessions into the room database
+ */
+class PianoSessionViewModel(private val sessionsRepository: SessionsRepository) : ViewModel() {
+    /**
+     * Holds current session ui state
+     */
+    var sessionUiState by mutableStateOf(SessionUiState())
+        private set
+}
 
 /*
 Represents the UI State for a Piano session
@@ -15,13 +29,33 @@ data class SessionDetails(
     val date: String = "",
     val timestamp: Long = 0L,
     val durationMillis: Long = 0L,
-    val pieceName: String = "",
-    val composer: String = "",
-    val bpm: String = "",
-    val measures: String = "",
+    val pieceName: String? = "",
+    val composer: String? = "",
+    val bpm: String? = "",
+    val measures: String? = "",
 )
 
+/**
+ * Extension function converting SessionDetails to PianoSession
+ * Saving a piano piece in a session is optional, therefore its details are optional,
+ * and thus nullable
+ */
+
 fun SessionDetails.toSession(): PianoSession = PianoSession(
+    id = id,
+    date = date,
+    timestamp = timestamp,
+    durationMillis = durationMillis,
+    pieceName = pieceName ?: "",
+    composer = composer ?: "",
+    bpm = bpm ?: "",
+    measures = measures ?: "",
+)
+
+/**
+ * Extension function converting PianoSession to SessionDetails
+ */
+fun PianoSession.toSessionDetails(): SessionDetails = SessionDetails(
     id = id,
     date = date,
     timestamp = timestamp,
@@ -31,3 +65,20 @@ fun SessionDetails.toSession(): PianoSession = PianoSession(
     bpm = bpm,
     measures = measures,
 )
+
+
+/**
+ * Extension function converting PianoSession to Session UI State
+ */
+fun PianoSession.toSessionUiState(isEntryValid: Boolean = false): SessionUiState = SessionUiState(
+    sessionDetails = this.toSessionDetails(),
+    isEntryValid = isEntryValid
+)
+
+private fun validateInput(uiState: SessionDetails = SessionDetails()): Boolean {
+    return with(uiState) {
+        date.isNotBlank() &&
+        timestamp.toString().isNotBlank() &&
+        durationMillis.toString().isNotBlank()
+    }
+}
